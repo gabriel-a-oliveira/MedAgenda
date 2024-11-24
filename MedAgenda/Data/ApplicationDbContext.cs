dc08babf3335670a3f9423e03fdbc1e3ba253da9
Gabriel Alves <gabrielalvesbosca@gmail.com>
2024-11-24 00:22:19 -0300

    Commit

--- MedAgenda/Data/ApplicationDbContext.cs
+++ MedAgenda/Data/ApplicationDbContext.cs
@@ -6,8 +6,25 @@ namespace MedAgenda.Data
     public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-        public DbSet<Medico> Medicos { get; set; }  
+        public DbSet<Medico> Medicos { get; set; }
         public DbSet<Paciente> Pacientes { get; set; }
         public DbSet<Consulta> Consultas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Consulta>()
+                .HasOne(c => c.Medico)
+                .WithMany()
+                .HasForeignKey(c => c.MedicoId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Consulta>()
+                .HasOne(c => c.Paciente)
+                .WithMany()
+                .HasForeignKey(c => c.PacienteId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
--- MedAgenda/Models/Consulta.cs
+++ MedAgenda/Models/Consulta.cs
@@ -1,16 +1,20 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MedAgenda.Models
 {
     public class Consulta
     {
+        [Key]
         public int Id { get; set; }
+        [ForeignKey("Medico")]
         public int MedicoId { get; set; }
+        [ForeignKey("Paciente")]
         public int PacienteId { get; set; }  
         [Required]
         public DateTime DataHora { get; set; } 
         [Required]
-        [StringLength(20)]
+        [RegularExpression("^(pendente|realizada|cancelada)$", ErrorMessage = "Status inválido")]
         public string? Status { get; set; }  
         public Medico? Medico { get; set; }
         public Paciente? Paciente { get; set; }  
